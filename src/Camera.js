7814c5964d1d9eca7b1931b272b93898b2f0c802
wwang178 <130918465+wwang178@users.noreply.github.com>
2024-05-11 09:56:08 -0700

    Add files via upload

--- src/Camera.js
+++ src/Camera.js
@@ -3,86 +3,96 @@ class Camera {
         this.eye = new Vector3([0, 0, 3]);
         this.at = new Vector3([0, 0, 100]);
         this.up = new Vector3([0, 1, 0]);
-        this.panAmount = 5;
+        this.panAmount = 1;
     }
 
-    forward() {
+    forward(distance) {
         let f = this.at.subNew(this.eye);
         f.normalize();
+        f.mul(distance);
         this.eye = this.eye.addNew(f);
         this.at = this.at.addNew(f);
     }
 
-    back() {
+    back(distance) {
         let f = this.at.subNew(this.eye);
         f.normalize();
+        f.mul(distance);
         this.eye = this.eye.subNew(f);
         this.at = this.at.subNew(f);
     }
 
-    left() {
+    left(distance) {
         let f = this.at.subNew(this.eye);
         f.normalize();
+        f.mul(distance);
         let s = Vector3.cross(f, this.up);
         s.normalize();
+        s.mul(distance);
         this.eye = this.eye.subNew(s);
         this.at = this.at.subNew(s);
     }
 
-    right() {
+    right(distance) {
         let f = this.at.subNew(this.eye);
         f.normalize();
+        f.mul(distance);
         let s = Vector3.cross(f, this.up);
         s.normalize();
+        s.mul(distance);
         this.eye = this.eye.addNew(s);
         this.at = this.at.addNew(s);
     }
 
-    moveUp() {
-        this.eye = this.eye.addNew(new Vector3([0, 1, 0]));
-        this.at = this.at.addNew(new Vector3([0, 1, 0]));
+    moveUp(distance) {
+        let moveVector = new Vector3([0, 1, 0]);
+        moveVector.mul(distance);
+        this.eye = this.eye.addNew(moveVector);
+        this.at = this.at.addNew(moveVector);
     }
 
-    moveDown() {
-        this.eye = this.eye.addNew(new Vector3([0, -1, 0]));
-        this.at = this.at.addNew(new Vector3([0, -1, 0]));
+    moveDown(distance) {
+        let moveVector = new Vector3([0, -1, 0]);
+        moveVector.mul(distance);
+        this.eye = this.eye.addNew(moveVector);
+        this.at = this.at.addNew(moveVector);
     }
 
-    panLeft() {
+    panLeft(multiplier) {
         let f = this.at.subNew(this.eye);
         let rMat = new Matrix4();
-        rMat.setRotate(this.panAmount, this.up.x, this.up.y, this.up.z);
+        rMat.setRotate(this.panAmount * multiplier, this.up.x, this.up.y, this.up.z);
         let fPrime = rMat.multiplyVector3(f);
         this.at = this.eye.addNew(fPrime);
     }
     
-    panRight() {
+    panRight(multiplier) {
         let f = this.at.subNew(this.eye);
         let rMat = new Matrix4();
-        rMat.setRotate(-this.panAmount, this.up.x, this.up.y, this.up.z);
+        rMat.setRotate(-this.panAmount * multiplier, this.up.x, this.up.y, this.up.z);
         let fPrime = rMat.multiplyVector3(f);
         this.at = this.eye.addNew(fPrime);
     }
 
-    panDown() {
+    panDown(multiplier) {
         let f = this.at.subNew(this.eye);
         let rightVector = Vector3.cross(this.up, f);
         rightVector.normalize();
     
         let rMat = new Matrix4();
-        rMat.setRotate(this.panAmount, rightVector.x, rightVector.y, rightVector.z);
+        rMat.setRotate(this.panAmount * multiplier, rightVector.x, rightVector.y, rightVector.z);
         
         let fPrime = rMat.multiplyVector3(f);
         this.at = this.eye.addNew(fPrime);
     }
 
-    panUp() {
+    panUp(multiplier) {
         let f = this.at.subNew(this.eye);
         let rightVector = Vector3.cross(this.up, f);
         rightVector.normalize();
     
         let rMat = new Matrix4();
-        rMat.setRotate(-this.panAmount, rightVector.x, rightVector.y, rightVector.z);
+        rMat.setRotate(-this.panAmount * multiplier, rightVector.x, rightVector.y, rightVector.z);
         
         let fPrime = rMat.multiplyVector3(f);
         this.at = this.eye.addNew(fPrime);
